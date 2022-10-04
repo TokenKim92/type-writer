236522a4a4ac000325f4586ede777beb8ef50112
Token Kim <TokenKim92@gmail.com>
2022-10-04 16:41:49 +0200

    add gitignore and webpack

--- typewriter/public/index.js
+++
@@ -1,5 +0,0 @@
-import TypeWriter from '../src/typewriter.js';
-
-new TypeWriter('typewriter', 50)
-  .type('Hi my name is Deokgeun Kim. It is my first project')
-  .start();
---
+++ webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path');
+const webpack = require('webpack');
+const pkg = require('./package.json');
+const isProduction = process.argv[process.argv.indexOf('--mode') + 1] === 'production'; //prettier-ignore
+const FILENAME = pkg.name + (isProduction ? '.min' : '');
+const BANNER = [
+  'Typewriter',
+  '@version ' + pkg.version + ' | ' + new Date().toDateString(),
+  '@author ' + pkg.author,
+  '@license ' + pkg.license,
+].join('\n');
+
+const config = {
+  entry: './src/typewriter.js',
+  output: {
+    path: path.join(__dirname, 'dist'),
+    filename: FILENAME + '.js',
+    library: ['TypeWriter'],
+    libraryTarget: 'umd',
+    libraryExport: 'default',
+  },
+  plugins: [
+    new webpack.BannerPlugin({
+      banner: BANNER,
+      entryOnly: true,
+    }),
+  ],
+};
+
+module.exports = config;
